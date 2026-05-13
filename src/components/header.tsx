56bd187c5a6818f1e5a823be7d2dca56ef7c2003
7Zac <isaac.lopesalencar@gmail.com>
2026-05-13 17:33:15 -0300

    chore: styled promotional features component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,17 @@ import SearchButton from "@/components/search-button"
 
 export default function Header() {
   return (
-    <header className="shadow-xl sticky top-0 bg-white z-50">
-      <div className="container mx-auto px-4 py-4">
+    <header className="shadow-xl sticky bg-white z-50">
+      <div className="container mx-auto py-2 px-2">
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center space-x-4">
             <Link href="/" className="flex items-center space-x-2">              
                 <img src="/logo_olinda.png" alt="Olinda Logo" className="h-10 w-auto" />
             </Link>
-            <div className="text-lg text-gray-600 border-l border-cyan-800 pl-4 flex flex-col items-center">
+            <div className="text-lg text-gray-600 border-l border-cyan-600 pl-4 flex flex-col items-center">
               <div className="font-semibold">Coleção</div>
-              <hr className="w-full text-cyan-800"/>
+              <hr className="w-full text-cyan-600"/>
               <div className="text-cyan-600 text-2xl font-[Cookie]">Inverno 2025</div>
             </div>
           </div>
--- src/components/promotional-features.tsx
+++ src/components/promotional-features.tsx
@@ -1,4 +1,5 @@
 import { CreditCard, Gift, Truck, Banknote, LineChart } from "lucide-react"
+import { Marquee } from "./ui/marquee"
 
 export default function PromotionalFeatures() {
   const features = [
@@ -29,33 +30,23 @@ export default function PromotionalFeatures() {
   ]
 
   return (
-    <section className="bg-cyan-800 py-12">
+    <section className="bg-cyan-600 py-8">
       <div className="container mx-auto px-4">
-        <div className="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 items-center">
-          {/* Linhas verticais centralizadas entre os ícones */}
-          <div className="hidden lg:flex flex-col items-center absolute left-1/4 top-1/2 -translate-y-1/2 z-10">
-            <hr className="border-white w-[90px] rotate-90 my-4" />
-          </div>
-          <div className="hidden lg:flex flex-col items-center absolute left-2/4 top-1/2 -translate-y-1/2 z-10">
-            <hr className="border-white w-[90px] rotate-90 my-4" />
-          </div>
-          <div className="hidden lg:flex flex-col items-center absolute left-3/4 top-1/2 -translate-y-1/2 z-10">
-            <hr className="border-white w-[90px] rotate-90 my-4" />
-          </div>
+        <Marquee pauseOnHover className="py-4">
           {features.map((feature, index) => (
-            <div key={index} className="text-center space-y-4 gap-4">
+            <div key={index} className="text-center space-y-4 gap-4 flex flex-col items-center px-8">
               <div
                 className={`inline-flex items-center justify-center w-16 h-16 rounded-full bg-white shadow-lg ${feature.color}`}
               >
                 <feature.icon className="w-8 h-8" />
               </div>
-              <div>
-                <h3 className="text-xl font-bold text-white">{feature.title}</h3>
-                <p className="text-orange-300">{feature.subtitle}</p>                
+              <div className="p-0">
+                <h3 className="text-5xl font-medium text-white font-['Cookie']">{feature.title}</h3>
+                <p className="text-orange-300 font-bold">{feature.subtitle}</p>                
               </div>                
             </div>
           ))}
-        </div>
+        </Marquee>
       </div>
       
     </section>
