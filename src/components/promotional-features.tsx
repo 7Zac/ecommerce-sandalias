936af282600e9f3fb6e7ddfd3a77698a72047a6d
7Zac <isaac.lopesalencar@gmail.com>
2026-07-09 15:25:58 -0300

    style: restyled promotional features

--- src/components/promotional-features.tsx
+++ src/components/promotional-features.tsx
@@ -13,42 +13,45 @@ export default function PromotionalFeatures() {
       icon: Gift,
       title: "OFF",
       subtitle: "10%",
-      color: "text-green-600",
+      color: "text-purple-600",
     },
     {
       icon: Truck,
       title: "Frete",
       subtitle: "Grátis",
-      color: "text-orange-600",
+      color: "text-yellow-600",
     },
     {
       icon: Banknote,
       title: "Cashback",
       subtitle: "acima de 500,00",
-      color: "text-purple-600",
+      color: "text-green-600",
     },
   ]
 
   return (
-    <section className="bg-slate-950 py-8">
-      <div className="container mx-auto px-4">
-        <Marquee pauseOnHover className="py-4">
-          {features.map((feature, index) => (
-            <div key={index} className="text-center space-y-4 gap-4 flex justify-center items-center px-8 border-l border-slate-700">
-              <div
-                className={`inline-flex items-center justify-center w-16 h-16 rounded-full shadow-lg ${feature.color}`}
-              >
-                <feature.icon className="w-12 h-12" />
-              </div>
-              <div className="p-0 flex flex-col items-start">
-                <h3 className="text-2xl font-bold text-white">{feature.title}</h3>
-                <p className="text-orange-300 text-xl font-bold">{feature.subtitle}</p>                
-              </div>                
-            </div>
-          ))}
-        </Marquee>
+    <div className="mt-16">
+      <div className="pt-[1px] pb-[1px] bg-gradient-to-r from-cyan-300 via-cyan-100 to-blue-500">
+        <section className=" bg-white py-6">
+          <div className="container mx-auto px-4">
+            <Marquee pauseOnHover className="">
+              {features.map((feature, index) => (
+                <div key={index} className="text-center flex justify-center items-center px-6">
+                  <div
+                    className={`inline-flex items-center justify-center w-16 h-16 rounded-full ${feature.color}`}
+                  >
+                    <feature.icon className="w-12 h-12" />
+                  </div>
+                  <div className="p-2 flex flex-col items-start">
+                    <h3 className="text-xl font-bold text-slate-950">{feature.title}</h3>
+                    <p className="text-orange-600 text-xl font-bold">{feature.subtitle}</p>
+                  </div>
+                </div>
+              ))}
+            </Marquee>
+          </div>
+        </section>
       </div>
-      
-    </section>
+    </div>
   )
 }
